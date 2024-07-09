fe23e97815d58e95789705d1870995b72bd21df5
NazarPetrakov <nazarpetrakov1@gmail.com>
2024-07-09 16:58:19 +0300

    Add project references, use repository pattern

---
+++ BookStore/BookStore.Application/Abstract/IRepository.cs
@@ -0,0 +1,8 @@
+﻿using BookStore.Domain.Models;
+
+namespace BookStore.Application.Abstract
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+    }
+}
---
+++ BookStore/BookStore.Infrastructure/Repositories/Repository.cs
@@ -0,0 +1,9 @@
+﻿using BookStore.Application.Abstract;
+using BookStore.Domain.Models;
+
+namespace BookStore.Infrastructure.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : BaseEntity
+    {
+    }
+}
